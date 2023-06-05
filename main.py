06e1107f954aa818d0c5348ef5d1f1a6f4edaad6
Domokos-Laszlo-Oliver <108230324+Domokos-Laszlo-Oliver@users.noreply.github.com>
2023-06-05 08:57:33 +0200

    Update main.py
    +

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from kivymd.app import MDApp
  #KivyMD DatePicker
 from kivymd.uix.pickers import MDDatePicker
 
+# Google Sheet communication
+import gspread
+ # Authenticate with Google Sheets
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 
@@ -94,7 +98,6 @@ class ReceiveMenu(Screen):
 
     def on_spinner_select(self, text):
         self.input_values_dictionary.update({"Anyag": text})
-        print(self.input_values_dictionary)
 
     #Datepicker
     # Átkellnézni meg átírni mert van benne feleslegs de még nem teljesen értem
@@ -117,8 +120,6 @@ class ReceiveMenu(Screen):
         date_range -> date_dialog= MDDatePicker(mode= "range") return a python list of the dates. [] Techically you can select a range of dates from 01 to 27.
         """
         # Access the button ID (button_id) here
-        print("Button ID:", button_id)
-        print(value)
         if button_id == "date_receive":
             converted_time = value.strftime('%Y-%m-%d')
             self.input_values_dictionary.update({"Beérkezés": converted_time})
@@ -128,47 +129,256 @@ class ReceiveMenu(Screen):
         else:
             converted_time = value.strftime('%Y-%m-%d')
             self.input_values_dictionary.update({"Lejárat": converted_time})
-        print(self.input_values_dictionary)
     def on_cancel(self, instance, value):
         pass
 
     def local_update(self):
-        productNumber = self.ids.product_number.text
+        product_number = self.ids.product_number.text
         quantity = self.ids.quantity.text
-        self.input_values_dictionary.update({"Gyártási szám": productNumber})
-        self.input_values_dictionary.update({"mennyiség": quantity})
+        self.input_values_dictionary.update({"Gyártási szám": product_number})
+        self.input_values_dictionary.update({"Mennyiség": quantity})
+        self.sync_List.append(self.input_values_dictionary)
+        #self.input_values_dictionary.clear()
         print(self.input_values_dictionary)
+        print(self.sync_List)
 
 
 
+    sync_List = []
     input_values_dictionary = {}
-    '''
-    def inputData(self):
-        self.input_values_dictionary.clear()
-        self.input_values_dictionary.update({"Részleg": selectedElement1.get()})
-        self.input_values_dictionary.update({"Anyag": selectedElement2.get()})
-        self.input_values_dictionary.update({"Beérkezés": arriveDateEntry.get()})
-        self.input_values_dictionary.update({"Gyártás": manufactureTimeEntry.get()})
-        self.input_values_dictionary.update(
-            {"Gyártási szám": manufactureNumberEntry.get()})
-        self.input_values_dictionary.update({"Lejárat": expiryDateEntry.get()})
-        self.input_values_dictionary.update({"Mennyiség": amountEntry.get()})
-        # unique Id a sheetsből készítve
-        syncList.append(input_values_dictionary)
-    '''
 
 
+    def sync_data(self, syncList = sync_List):
+          scope = [
+            'https://spreadsheets.google.com/feeds',
+            'https://www.googleapis.com/auth/drive'
+          ]
+          credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            'rich-sprite.json', scope)
+          client = gspread.authorize(credentials)
 
+          # x axison megy örökké
+          #16384 XFD
+          def convertUniqueIDToColumn(uniqueID):
+            dividend = uniqueID
+            column_name = ''
 
+            while dividend > 0:
+              modulo = (dividend - 1) % 26
+              column_name = chr(65 + modulo) + column_name
+              dividend = (dividend - modulo) // 26
 
+            return column_name
 
+          for item in syncList:
+            if item["Részleg"] == 'Általános':
+              # Open an existing Google Sheets spreadsheet
+              sheet = client.open("Inventory Management App").worksheet(
+                "Általános")
+              sheet.add_cols(1)
+              uniqueID = int(sheet.cell(1, 1).value[1:])
+              columnLetter = convertUniqueIDToColumn(uniqueID)
+              #ide hogy mitötrténjen vele
 
+              sheet.update_cell(1, uniqueID, "G" + columnLetter)  #mit column
+              sheet.update_cell(2, uniqueID, item["Anyag"])
+              sheet.update_cell(3, uniqueID, item["Beérkezés"])
+              sheet.update_cell(4, uniqueID, item["Gyártás"])
+              sheet.update_cell(5, uniqueID, item["Gyártási szám"])
+              sheet.update_cell(6, uniqueID, item["Lejárat"])
+              sheet.update_cell(7, uniqueID, item["Mennyiség"])
+              #Img
 
+              #ebbe megy a jelenlegi adat
+              uniqueID = uniqueID + 1
+              sheet.update_cell(1, 1, "G" + str(uniqueID))
+
+            if item["Részleg"] == 'Termelés':
+              # Open an existing Google Sheets spreadsheet
+              sheet = client.open("Inventory Management App").worksheet("Termelés")
+              sheet.add_cols(1)
+              uniqueID = int(sheet.cell(1, 1).value[1:])
+              columnLetter = convertUniqueIDToColumn(uniqueID)
+
+              #ide hogy mitötrténjen vele
+
+              sheet.update_cell(1, uniqueID, "P" + columnLetter)  #mit column
+              sheet.update_cell(2, uniqueID, item["Anyag"])
+              sheet.update_cell(3, uniqueID, item["Beérkezés"])
+              sheet.update_cell(4, uniqueID, item["Gyártás"])
+              sheet.update_cell(5, uniqueID, item["Gyártási szám"])
+              sheet.update_cell(6, uniqueID, item["Lejárat"])
+              sheet.update_cell(7, uniqueID, item["Mennyiség"])
+              #Img
+
+              #ebbe megy a jelenlegi adat
+              uniqueID = uniqueID + 1
+              sheet.update_cell(1, 1, "P" + str(uniqueID))
+
+            if item["Részleg"] == 'Donorterem':
+              # Open an existing Google Sheets spreadsheet
+              sheet = client.open("Inventory Management App").worksheet(
+                "Donorterem")
+              sheet.add_cols(1)
+              uniqueID = int(sheet.cell(1, 1).value[1:])
+              columnLetter = convertUniqueIDToColumn(uniqueID)
+              #ide hogy mitötrténjen vele
+
+              sheet.update_cell(1, uniqueID, "D" + columnLetter)  #mit column
+              sheet.update_cell(2, uniqueID, item["Anyag"])
+              sheet.update_cell(3, uniqueID, item["Beérkezés"])
+              sheet.update_cell(4, uniqueID, item["Gyártás"])
+              sheet.update_cell(5, uniqueID, item["Gyártási szám"])
+              sheet.update_cell(6, uniqueID, item["Lejárat"])
+              sheet.update_cell(7, uniqueID, item["Mennyiség"])
+              #Img
+
+              #ebbe megy a jelenlegi adat
+              uniqueID = uniqueID + 1
+              sheet.update_cell(1, 1, "D" + str(uniqueID))
+
+            if item["Részleg"] == 'Recepció':
+              # Open an existing Google Sheets spreadsheet
+              sheet = client.open("Inventory Management App").worksheet("Recepció")
+              sheet.add_cols(1)
+              uniqueID = int(sheet.cell(1, 1).value[1:])
+              columnLetter = convertUniqueIDToColumn(uniqueID)
+              #ide hogy mitötrténjen vele
+
+              sheet.update_cell(1, uniqueID, "R" + columnLetter)  #mit column
+              sheet.update_cell(2, uniqueID, item["Anyag"])
+              sheet.update_cell(3, uniqueID, item["Beérkezés"])
+              sheet.update_cell(4, uniqueID, item["Gyártás"])
+              sheet.update_cell(5, uniqueID, item["Gyártási szám"])
+              sheet.update_cell(6, uniqueID, item["Lejárat"])
+              sheet.update_cell(7, uniqueID, item["Mennyiség"])
+              #Img
+
+              #ebbe megy a jelenlegi adat
+              uniqueID = uniqueID + 1
+              sheet.update_cell(1, 1, "R" + str(uniqueID))
+
+          syncList.clear()
 
 
 
 class ExpendMenu(Screen):
-    pass
+
+    expendList = []
+
+    def materialExpend(self):
+        expendMaterialDictionary = {}
+        expendMaterialDictionary.update({"ID": self.ids.expend_id.text})
+        expendMaterialDictionary.update({"Quantity": self.ids.expend_quantity.text})
+        self.expendList.append(expendMaterialDictionary)
+        print(self.expendList)
+
+
+
+    def eSychronizeData(self, syncList = expendList):
+
+        def convertColumnToUniqueID(column_name):
+            num = 0
+            for c in column_name:
+                if c.isalpha():
+                    num = num * 26 + (ord(c.upper()) - ord('A')) + 1
+            return num
+
+        scope = [
+            'https://spreadsheets.google.com/feeds',
+            'https://www.googleapis.com/auth/drive'
+        ]
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            'rich-sprite.json', scope)
+        client = gspread.authorize(credentials)
+
+        for item in syncList:
+            print(syncList)
+            worksheet = item["ID"][0]
+            id = item["ID"][1:]
+
+            quantity = int(item["Quantity"])
+
+            if worksheet == "G":
+                # Open an existing Google Sheets spreadsheet
+                sheet = client.open("Inventory Management App").worksheet(
+                    "Általános")
+                columnInt = convertColumnToUniqueID(id)
+                rowValue = sheet.cell(7, columnInt).value
+                print(rowValue)
+                sheet.update_cell(7, columnInt, int(rowValue) - quantity)
+
+            elif worksheet == "P":
+                # Open an existing Google Sheets spreadsheet
+                sheet = client.open("Inventory Management App").worksheet("Termelés")
+                columnInt = convertColumnToUniqueID(id)
+                rowValue = sheet.cell(7, columnInt).value
+                print(rowValue)
+                sheet.update_cell(7, columnInt, int(rowValue) - quantity)
+            elif worksheet == "D":
+                # Open an existing Google Sheets spreadsheet
+                sheet = client.open("Inventory Management App").worksheet(
+                    "Donorterem")
+                columnInt = convertColumnToUniqueID(id)
+                rowValue = sheet.cell(7, columnInt).value
+                print(rowValue)
+                sheet.update_cell(7, columnInt, int(rowValue) - quantity)
+            else:
+                # Open an existing Google Sheets spreadsheet
+                sheet = client.open("Inventory Management App").worksheet("Recepció")
+                columnInt = convertColumnToUniqueID(id)
+                rowValue = sheet.cell(7, columnInt).value
+                print(rowValue)
+                sheet.update_cell(7, columnInt, int(rowValue) - quantity)
+
+            # ide hogy mitötrténjen vele
+
+    expendList.clear()
+    print(len(expendList))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class WindowManager(ScreenManager):
