9d20f221b72764600c83eda345a56156fe244797
Domokos-Laszlo-Oliver <108230324+Domokos-Laszlo-Oliver@users.noreply.github.com>
2023-06-08 13:32:08 +0200

    Update main.py
    +

--- main.py
+++ main.py
@@ -24,49 +24,49 @@ class MainMenu(Screen):
 
 class ReceiveMenu(Screen):
     generalList = [
-        "FERŐTLENÍTŐ----------", "Brado Clean 1L", "Brado Clean 5L",
+        "----------FERŐTLENÍTŐ----------", "Brado Clean 1L", "Brado Clean 5L",
         "Brado Plus 1000ML", "Brado Plus 250ML", "Bradogél 500ML",
         "Bradogél 1000ML", "Bradosept 1L", "Bradosept 5L", "Mikorzid AF 100ML",
         "Mikrozid szórófej", "Mikrozid dobozos törlőkendő 150x",
         "Mikrozid dobozos törlőkendő utántöltő 150x", "Sekusept Aktív",
         "Skinman Soft Protect", "Skinman Soft Plus", "Skinsan Scrub",
-        "Imi Orange", "Melsept 1000ML", "", "KESZTYŰ----------", "XL Kesztyű",
-        "L Kesztyű", "M Kesztyű", "S Kesztyű", "", "CÍMKÉK----------",
+        "Imi Orange", "Melsept 1000ML", "----------KESZTYŰ----------", "XL Kesztyű",
+        "L Kesztyű", "M Kesztyű", "S Kesztyű", "----------CÍMKÉK----------",
         "500-as címke", "80x80 donor címke hűtőházas", "Donorkártya címke 80x50",
-        "1000-es címke (Mintacső)", "150x80 dobozos címkehűtőházas", "",
-        "TAKARÍTÓ----------", "Lucart Jumbo WC papír 12x (donor?)",
+        "1000-es címke (Mintacső)", "150x80 dobozos címkehűtőházas",
+        "----------TAKARÍTÓ----------", "Lucart Jumbo WC papír 12x (donor?)",
         "Lucart kéztörlő 155 ID", "Lucart kéztörlő henger 140A",
         "Lucart WC papír 10x (személyzeti)", "Lucart Z kéztörlő 15x",
-        "Tork Illatosító", "Tork folyékony szappan", "", "EGYÉB----------",
+        "Tork Illatosító", "Tork folyékony szappan", "----------EGYÉB----------",
         "Szájmaszk", "Szájmaszk FFP2", "Fénymásoló papír 500x", "Vizes Ballon",
         "Műanyag pohár"
     ]
 
     receptionList = [
-        "AJÁNDÉK----------", "Biotech USA műzli", "Belvita", "Sportszelet",
-        "Swiss ital", "", "EGYÉB----------", "Függőmappa", "Függőmappa lefűző",
+        "----------AJÁNDÉK----------", "Biotech USA műzli", "Belvita", "Sportszelet",
+        "Swiss ital", "----------EGYÉB----------", "Függőmappa", "Függőmappa lefűző",
         "Lábzsák", "Archiváló doboz"
     ]
 
     donorList = [
-        "CSÖVEK----------", "BD Vacutainer", "Biztonsági tű 21G", "NAT 4ML",
+        "----------CSÖVEK----------", "BD Vacutainer", "Biztonsági tű 21G", "NAT 4ML",
         "NAT 6ML", "S-Monovette Fehér", "S-Monovette Zöld", "S-Monovette Lila",
-        "S-Monovette Piros", "Vérvételi harang", "", "EGYÉB----------",
+        "S-Monovette Piros", "Vérvételi harang", "----------EGYÉB----------",
         "Hányós zacskó", "Papírlepedő", "Omnisilk", "Pur-Zellin", "Peha-Haft"
     ]
 
     productionList = [
-        "FŐ SEGÉDANYAG----------", "Szerelék ZBK", "Szerelék 4117",
+        "----------FŐ SEGÉDANYAG----------", "Szerelék ZBK", "Szerelék 4117",
         "Egyszer használatos kanül", "NaCl - Fresenius 250ML",
         "NaCl - Fresenius 500ML", "NaCl - B. Braun", "Citrát - Fresenius",
         "Citrát - Medites Pharma", "Citrát - Macopharma", "Citrát - Haemonetics",
-        "", "SYSMEX----------", "Cellclean 50ML", "Cellpack 20L",
-        "Stromatolyser", "Kontrollvér Low", "Kontrollvér Normal", "",
-        "VESZÉLYES HULLADÉK----------"
+        "----------SYSMEX----------", "Cellclean 50ML", "Cellpack 20L",
+        "Stromatolyser", "Kontrollvér Low", "Kontrollvér Normal",
+        "----------VESZÉLYES HULLADÉK----------"
         "Veszélyes hulladék kanna", "Veszélyes hulladék kuka 2L",
         "Veszélyes hulladék 5L", "Veszélyes hulladék 10L",
         "Veszélyes hulladék 20L", "Veszélyes hulladék 60L",
-        "Veszélyes hulladék zsák", "", "EGYÉB----------",
+        "Veszélyes hulladék zsák", "----------EGYÉB----------",
         "Egyszerhasználatos köpeny", "Hőpapír", "Élvédő"
                                                 "Gyorskötegelő", "NAT hungarocell", "NAT hungarocell nylon",
         "Raklapfólia", "Plazmatároló M5 rekesz fagyasztókamrához", "Élvédő"
@@ -335,48 +335,6 @@ class ExpendMenu(Screen):
 
     expendList.clear()
     print(len(expendList))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
