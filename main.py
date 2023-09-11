9bcba79fcd0802bb1b7c286afc9e409c2e55f819
Domokos-Laszlo-Oliver <108230324+Domokos-Laszlo-Oliver@users.noreply.github.com>
2023-09-11 13:27:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # Kivy
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
-
- # KivyMD DatePicke
+# KivyMD for Datepicker
 from kivymd.app import MDApp
- #KivyMD DatePicker
+# KivyMD DatePicker module
 from kivymd.uix.pickers import MDDatePicker
+#Popup
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.uix.scrollview import ScrollView
 
 # Google Sheet communication
 import gspread
@@ -15,72 +19,70 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 
-
-# Define screens
-
+# Defining screens
 class MainMenu(Screen):
     pass
 
 
 class ReceiveMenu(Screen):
     generalList = [
-        "----------FERŐTLENÍTŐ----------", "Brado Clean 1L", "Brado Clean 5L",
+        "FERŐTLENÍTŐ----------", "Brado Clean 1L", "Brado Clean 5L",
         "Brado Plus 1000ML", "Brado Plus 250ML", "Bradogél 500ML",
         "Bradogél 1000ML", "Bradosept 1L", "Bradosept 5L", "Mikorzid AF 100ML",
         "Mikrozid szórófej", "Mikrozid dobozos törlőkendő 150x",
         "Mikrozid dobozos törlőkendő utántöltő 150x", "Sekusept Aktív",
         "Skinman Soft Protect", "Skinman Soft Plus", "Skinsan Scrub",
-        "Imi Orange", "Melsept 1000ML", "----------KESZTYŰ----------", "XL Kesztyű",
-        "L Kesztyű", "M Kesztyű", "S Kesztyű", "----------CÍMKÉK----------",
+        "Imi Orange", "Melsept 1000ML", "", "KESZTYŰ----------", "XL Kesztyű",
+        "L Kesztyű", "M Kesztyű", "S Kesztyű", "", "CÍMKÉK----------",
         "500-as címke", "80x80 donor címke hűtőházas", "Donorkártya címke 80x50",
-        "1000-es címke (Mintacső)", "150x80 dobozos címkehűtőházas",
-        "----------TAKARÍTÓ----------", "Lucart Jumbo WC papír 12x (donor?)",
+        "1000-es címke (Mintacső)", "150x80 dobozos címkehűtőházas", "",
+        "TAKARÍTÓ----------", "Lucart Jumbo WC papír 12x (donor?)",
         "Lucart kéztörlő 155 ID", "Lucart kéztörlő henger 140A",
         "Lucart WC papír 10x (személyzeti)", "Lucart Z kéztörlő 15x",
-        "Tork Illatosító", "Tork folyékony szappan", "----------EGYÉB----------",
+        "Tork Illatosító", "Tork folyékony szappan", "", "EGYÉB----------",
         "Szájmaszk", "Szájmaszk FFP2", "Fénymásoló papír 500x", "Vizes Ballon",
         "Műanyag pohár"
     ]
 
     receptionList = [
-        "----------AJÁNDÉK----------", "Biotech USA műzli", "Belvita", "Sportszelet",
-        "Swiss ital", "----------EGYÉB----------", "Függőmappa", "Függőmappa lefűző",
+        "AJÁNDÉK----------", "Biotech USA műzli", "Belvita", "Sportszelet",
+        "Swiss ital", "", "EGYÉB----------", "Függőmappa", "Függőmappa lefűző",
         "Lábzsák", "Archiváló doboz"
     ]
 
     donorList = [
-        "----------CSÖVEK----------", "BD Vacutainer", "Biztonsági tű 21G", "NAT 4ML",
+        "CSÖVEK----------", "BD Vacutainer", "Biztonsági tű 21G", "NAT 4ML",
         "NAT 6ML", "S-Monovette Fehér", "S-Monovette Zöld", "S-Monovette Lila",
-        "S-Monovette Piros", "Vérvételi harang", "----------EGYÉB----------",
+        "S-Monovette Piros", "Vérvételi harang", "", "EGYÉB----------",
         "Hányós zacskó", "Papírlepedő", "Omnisilk", "Pur-Zellin", "Peha-Haft"
     ]
 
     productionList = [
-        "----------FŐ SEGÉDANYAG----------", "Szerelék ZBK", "Szerelék 4117",
+        "FŐ SEGÉDANYAG----------", "Szerelék ZBK", "Szerelék 4117",
         "Egyszer használatos kanül", "NaCl - Fresenius 250ML",
         "NaCl - Fresenius 500ML", "NaCl - B. Braun", "Citrát - Fresenius",
         "Citrát - Medites Pharma", "Citrát - Macopharma", "Citrát - Haemonetics",
-        "----------SYSMEX----------", "Cellclean 50ML", "Cellpack 20L",
-        "Stromatolyser", "Kontrollvér Low", "Kontrollvér Normal",
-        "----------VESZÉLYES HULLADÉK----------"
+        "", "SYSMEX----------", "Cellclean 50ML", "Cellpack 20L",
+        "Stromatolyser", "Kontrollvér Low", "Kontrollvér Normal", "",
+        "VESZÉLYES HULLADÉK----------"
         "Veszélyes hulladék kanna", "Veszélyes hulladék kuka 2L",
         "Veszélyes hulladék 5L", "Veszélyes hulladék 10L",
         "Veszélyes hulladék 20L", "Veszélyes hulladék 60L",
-        "Veszélyes hulladék zsák", "----------EGYÉB----------",
+        "Veszélyes hulladék zsák", "", "EGYÉB----------",
         "Egyszerhasználatos köpeny", "Hőpapír", "Élvédő"
                                                 "Gyorskötegelő", "NAT hungarocell", "NAT hungarocell nylon",
         "Raklapfólia", "Plazmatároló M5 rekesz fagyasztókamrához", "Élvédő"
     ]
 
-
-
-
-
+    sync_List = []
+    input_values_dictionary = {}
 
     #ReceiveMenu Dropdown spinner logic
     def spinner_clicked(self, value):
         """
-        Logic for changing the values in item_spinner by category_spinner
+        Logic for changing the values in item_spinner by interacting with category_spinner.
+        When clicked it is going to change the naming of both itself and the other spinner but also change the contents of the secondary spinner which will contain all the chooseable items.
+        On click event the button is going to give itself back the chosen value thus it know what to display in both dropdown menus.
         """
         if value == "Általános":
             self.ids.item_spinner.values = self.generalList
@@ -99,8 +101,9 @@ class ReceiveMenu(Screen):
     def on_spinner_select(self, text):
         self.input_values_dictionary.update({"Anyag": text})
 
-    #Datepicker
-    # Átkellnézni meg átírni mert van benne feleslegs de még nem teljesen értem
+
+
+    #Datepicker----------------------------------------------------------------------------------
     def show_date_picker(self, title, button_id):
         """
         Makes DatePicker Widget, binds on_save/on_cancel function to it, and opens it.
@@ -117,35 +120,43 @@ class ReceiveMenu(Screen):
         def on_save(self, instance, value, date_range, ids):
         instance ->
         value -> The date data itself. (2777-07-07)
-        date_range -> date_dialog= MDDatePicker(mode= "range") return a python list of the dates. [] Techically you can select a range of dates from 01 to 27.
+        date_range -> date_dialog= MDDatePicker(mode= "range") return a python list of the dates. Techically you can select a range of dates from 01 to 27.
+
+        !!!!!!!!!!!!!!! Solve it so it dispalys the date next to the text on the button!!!!!!!!
         """
         # Access the button ID (button_id) here
         if button_id == "date_receive":
             converted_time = value.strftime('%Y-%m-%d')
             self.input_values_dictionary.update({"Beérkezés": converted_time})
+            #self.ids.date_receive.text = f'Beérkezés: {converted_time}'
         elif button_id == "date_production":
             converted_time = value.strftime('%Y-%m-%d')
             self.input_values_dictionary.update({"Gyártás": converted_time})
+            #self.ids.date_production.text = f'Gyártás: {converted_time}'
         else:
             converted_time = value.strftime('%Y-%m-%d')
             self.input_values_dictionary.update({"Lejárat": converted_time})
+            #self.ids.date_expiry.text = f'Lejárat: {converted_time}'
     def on_cancel(self, instance, value):
-        pass
+        return
 
+
+
+
+    # Local update and Sync Update Buttons-----------------------------------------------------
     def local_update(self):
-        product_number = self.ids.product_number.text
-        quantity = self.ids.quantity.text
-        self.input_values_dictionary.update({"Gyártási szám": product_number})
-        self.input_values_dictionary.update({"Mennyiség": quantity})
+        """
+        Needed because the text field does not have an onclick event to put their values into the dictionary.
+        Also with SyncList and input_values_dictionary data can be stored offline in case there is no internet or to synchronise multiple data.
+        """
+        self.input_values_dictionary.update({"Gyártási szám": self.ids.product_number.text})
+        self.input_values_dictionary.update({"Mennyiség": self.ids.quantity.text})
         self.sync_List.append(self.input_values_dictionary)
         #self.input_values_dictionary.clear()
         print(self.input_values_dictionary)
         print(self.sync_List)
 
 
-
-    sync_List = []
-    input_values_dictionary = {}
 
 
     def sync_data(self, syncList = sync_List):
@@ -157,9 +168,13 @@ class ReceiveMenu(Screen):
             'rich-sprite.json', scope)
           client = gspread.authorize(credentials)
 
-          # x axison megy örökké
-          #16384 XFD
+
           def convertUniqueIDToColumn(uniqueID):
+            """
+            Formula for converting the unique ID-s to columns in Google Sheets on the X axis.
+            Max number is 16384 XFD apparently but it never got tested.
+            """
+
             dividend = uniqueID
             column_name = ''
 
@@ -260,6 +275,51 @@ class ReceiveMenu(Screen):
 
           syncList.clear()
 
+    def show_popup_local(self, instance, list = input_values_dictionary):
+        # Create content for the popup
+        box = BoxLayout(orientation='vertical')
+        label = Label(text=str(list))
+        btn = Button(text='Bezár', size_hint_y=None, height=44)
+
+        box.add_widget(label)
+        box.add_widget(btn)
+
+        # Create the popup
+        popup = Popup(title='Helyileg bevételezett anyagok', content=box, size_hint=(0.8, 0.4))
+
+        # Bind the close button to dismiss the popup
+        btn.bind(on_release=popup.dismiss)
+
+        # Open the popup
+        popup.open()
+
+    from kivy.uix.scrollview import ScrollView
+
+    def show_popup_sync(self, instance, list=sync_List):
+        # Create content for the popup
+        box = BoxLayout(orientation='vertical')
+
+        # Create the ScrollView
+        scroll = ScrollView(do_scroll_x=False)  # We disable horizontal scrolling
+
+        # Content to be made scrollable
+        label = Label(text=str(list), size_hint_y=None)
+        label.height = label.texture_size[1]  # Adjust the height of the label to its content
+
+        scroll.add_widget(label)  # Add label to the ScrollView
+        box.add_widget(scroll)  # Add ScrollView to the main layout
+
+        btn = Button(text='Close', size_hint_y=None, height=44)
+        box.add_widget(btn)
+
+        # Create the popup
+        popup = Popup(title='Helyileg bevételezett anyagok', content=box, size_hint=(0.8, 0.4))
+
+        # Bind the close button to dismiss the popup
+        btn.bind(on_release=popup.dismiss)
+
+        # Open the popup
+        popup.open()
 
 
 class ExpendMenu(Screen):
@@ -335,6 +395,7 @@ class ExpendMenu(Screen):
 
     expendList.clear()
     print(len(expendList))
+
 
 
 
